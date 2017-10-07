2d1b2046d9c3ad656b601bd0102932c6395852e1
DmitryKMRK <dmitry.kmrk@gmail.com>
2017-10-07 07:59:21 +0300

    Desktop design

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+var wait = require("gulp-wait");
+
+gulp.task("style", function() {
+  gulp.src("sass/style.scss")
+    .pipe(wait(500))
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer({browsers: [
+        "last 2 versions"
+      ]})
+    ]))
+    .pipe(gulp.dest("css"))
+    .pipe(server.stream());
+});
+
+gulp.task("serve", ["style"], function() {
+  server.init({
+    server: ".",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("sass/**/*.{scss,sass}", ["style"]);
+  gulp.watch("*.html").on("change", server.reload);
+});
