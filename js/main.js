0e9f0a78679e4b32e45e688a1b9dda82daf01249
DmitryKMRK <dmitry.kmrk@gmail.com>
2017-10-09 12:09:08 +0300

    Slider

--- js/main.js
+++ js/main.js
@@ -23,26 +23,44 @@ $(function() {
 });
 
 //Slider-arrows
-function slider(elementClass, prevBtnClass, nextBtnClass, activeClass) {
+function slider(elementClass, prevBtnClass, nextBtnClass, activeElementClass, controlClass, activeControlClass) {
 	var sliderItem = document.querySelectorAll('.' + elementClass);
 	var prevArrow = document.querySelector('.' + prevBtnClass);
 	var nextArrow = document.querySelector('.' + nextBtnClass);
 	var firstSlide = sliderItem[0];
 	var lastSlide = sliderItem[sliderItem.length - 1];
+	var controlItem = document.querySelectorAll('.' + controlClass);
+	var firstControl = controlItem[0];
+	var lastControl = controlItem[controlItem.length - 1];
 
 	nextArrow.addEventListener('click', function() {
 		for(var i = 0; i < sliderItem.length; i++) {
 			var currentSlide = sliderItem[i];
 			var nextSlide = currentSlide.nextElementSibling;
 
-			if(currentSlide.classList.contains(activeClass)) {
-				currentSlide.classList.remove(activeClass);
-	            if(currentSlide == lastSlide) {
-	              firstSlide.classList.add(activeClass);
-	            } else {
-	              nextSlide.classList.add(activeClass);
-	            }
-	            break;
+			if(currentSlide.classList.contains(activeElementClass)) {
+				currentSlide.classList.remove(activeElementClass);
+        if(currentSlide == lastSlide) {
+          firstSlide.classList.add(activeElementClass);
+        } else {
+          nextSlide.classList.add(activeElementClass);
+        }
+        break;
+			}
+		}
+
+		for(var i = 0; i < controlItem.length; i++) {
+			var currentControl = controlItem[i];
+			var nextControl = currentControl.nextElementSibling;
+
+			if(currentControl.classList.contains(activeControlClass)) {
+				currentControl.classList.remove(activeControlClass);
+        if(currentControl == lastControl) {
+          firstControl.classList.add(activeControlClass);
+        } else {
+          nextControl.classList.add(activeControlClass);
+        }
+        break;
 			}
 		}
 	});
@@ -52,20 +70,35 @@ function slider(elementClass, prevBtnClass, nextBtnClass, activeClass) {
 			var currentSlide = sliderItem[i];
 			var prevSlide = currentSlide.previousElementSibling;
 
-			if(currentSlide.classList.contains(activeClass)) {
-				currentSlide.classList.remove(activeClass);
-	            if(currentSlide == firstSlide) {
-	              lastSlide.classList.add(activeClass);
-	            } else {
-	              prevSlide.classList.add(activeClass);
-	            }
-	            break;
+			if(currentSlide.classList.contains(activeElementClass)) {
+				currentSlide.classList.remove(activeElementClass);
+        if(currentSlide == firstSlide) {
+          lastSlide.classList.add(activeElementClass);
+        } else {
+          prevSlide.classList.add(activeElementClass);
+        }
+        break;
+			}
+		}
+
+		for(var i = 0; i < controlItem.length; i++) {
+			var currentControl = controlItem[i];
+			var prevControl = currentControl.previousElementSibling;
+
+			if(currentControl.classList.contains(activeControlClass)) {
+				currentControl.classList.remove(activeControlClass);
+        if(currentControl == firstControl) {
+          lastControl.classList.add(activeControlClass);
+        } else {
+          prevControl.classList.add(activeControlClass);
+        }
+        break;
 			}
 		}
 	});
 }
 
-slider('reviews__slide', 'reviews__slider-arrow--left', 'reviews__slider-arrow--right', 'reviews__slide--active');
+slider('reviews__slide', 'reviews__slider-arrow--left', 'reviews__slider-arrow--right', 'reviews__slide--active', 'reviews__slider-control', 'reviews__slider-control--active');
 
 //Slider-controls
 function sliderControls(controlClass, slideClass, activeControlClass, activeSlideClass) {
