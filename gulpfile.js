80b82795d187cb512f72b6d18fb1108212661156
DmitryKMRK <dmitry.kmrk@gmail.com>
2017-10-09 12:21:16 +0300

    Build

--- gulpfile.js
+++ gulpfile.js
@@ -5,32 +5,78 @@ var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
 var server = require("browser-sync").create();
-var wait = require("gulp-wait");
+var run = require("run-sequence");
+var del = require("del");
+
+gulp.task("build", function(fn) {
+  run("clean", "copy", "style", "images", fn);
+});
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+    "img/**",
+    "js/**",
+    "*.html"
+  ], {
+    base: "."
+  })
+  .pipe(gulp.dest("build"));
+});
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
-    .pipe(wait(500))
     .pipe(plumber())
     .pipe(sass())
     .pipe(postcss([
       autoprefixer({browsers: [
         "last 2 versions"
-      ]})
+      ]}),
+      mqpacker()
     ]))
-    .pipe(gulp.dest("css"))
-    .pipe(server.stream());
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"));
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("images", function() {
+  return gulp.src("build/img/**/*.{png,jpg,gif}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true})
+    ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("serve", function() {
   server.init({
-    server: ".",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
 
+  gulp.task("html:copy", function() {
+    return gulp.src("*.html")
+    .pipe(gulp.dest("build"));
+  });
+
+  gulp.task("html:update", ["html:copy"], function(done) {
+    server.reload();
+    done();
+  });
+
   gulp.watch("sass/**/*.{scss,sass}", ["style"]);
-  gulp.watch("*.html").on("change", server.reload);
+  gulp.watch("*.html", ["html:update"]);
 });
